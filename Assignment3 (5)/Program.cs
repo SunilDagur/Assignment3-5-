58f4730129e1dcc60d4907019eddae2dac305fb0
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:14:56 +0530

    Add project files.

---
+++ Assignment3 (5)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using AssemblyA;
+
+[assembly: InternalsVisibleTo("AssemblyB")]
+
+namespace AssemblyA
+{
+    public class MyClass
+    {
+        protected internal int protectedInternalField = 66;
+
+        protected internal void ProtectedInternalMethod()
+        {
+            Console.WriteLine("This is a protected internal method.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        Console.WriteLine("Accessing Protected Internal Field from Assembly B: " + myObject.protectedInternalField);
+        myObject.ProtectedInternalMethod();
+    }
+}
